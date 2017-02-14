b2386216b6ddeb204c5223de6dedc89640a50af8
Nemo <nemoicap@gmail.com>
2017-02-14 19:02:45 +0300

    Add efix library

--- ImageApplication/Web.ImageApplication/Controllers/HomeController.cs
+++ ImageApplication/Web.ImageApplication/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MetadataExtractor;
 
 namespace Web.ImageApplication.Controllers
 {
@@ -11,7 +13,11 @@ namespace Web.ImageApplication.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var dir = Server.MapPath("/Content/images");
+            var path = Path.Combine(dir, "iguana" + ".jpg");
+            var elem = base.File(path, "image/jpeg");
+            var directories = ImageMetadataReader.ReadMetadata(path);
+            return base.File(path, "image/jpeg");
         }
     }
 }
