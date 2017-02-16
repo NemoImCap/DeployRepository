f49c626572979e48a3c864f5209a90f3387f3be0
Nemo <nemoicap@gmail.com>
2017-02-17 02:09:26 +0300

    change working with map

--- ImageApplication/Web.ImageApplication/Scripts/scripts.js
+++ ImageApplication/Web.ImageApplication/Scripts/scripts.js
@@ -1,25 +1,18 @@
-﻿function initMap(lat, lon) {
-    var myLatLng = { lat: -25.363, lng: 131.044 };
-
-    var map = new google.maps.Map(document.getElementById('home_map_canvas'), {
-        zoom: 2,
-        center: myLatLng
-    });
-}
+﻿var map;
+var marker;
 
 function setMarker(lat, lon) {
     var getLocationMap = { lat: Number(lat.toFixed(3)), lng: Number(lon.toFixed(3)) };
-
-    var map = new google.maps.Map(document.getElementById('home_map_canvas'), {
-        zoom: 3,
+    map = new google.maps.Map(document.getElementById('home_map_canvas'), {
+        zoom: 2,
         center: getLocationMap
     });
-    var myLatLng = { lat: Number(lat.toFixed(3)), lng: Number(lon.toFixed(3)) };
-    var marker = new google.maps.Marker({
-        position: myLatLng,
+    marker = new google.maps.Marker({
         map: map,
+        position: getLocationMap,
         title: 'Was here!'
     });
+    marker.setMap(map);
 }
 
 
@@ -27,4 +20,3 @@ var toDecimal = function (number) {
     return number[0].numerator + number[1].numerator /
         (60 * number[1].denominator) + number[2].numerator / (3600 * number[2].denominator);
 };
-initMap();
